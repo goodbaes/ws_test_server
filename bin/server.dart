6e8a215089f9379bddf2c9b1f3023bd4ad502d87
Ruslan <95baes@gmail.com>
2022-07-30 14:40:56 +0600

    new ip

--- bin/server.dart
+++ bin/server.dart
@@ -49,14 +49,12 @@ Response _echoHandler(Request request) {
 
 void main(List<String> args) async {
   redisConnection = RedisConnection();
-  command = await redisConnection.connect("localhost", 6379);
-  // Use any available host or container IP (usually `0.0.0.0`).
   final ip = InternetAddress.anyIPv4;
 
-  // Configure a pipeline that logs requests.
+  command = await redisConnection.connect(ip.address, 6379);
+
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
 
-  // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
